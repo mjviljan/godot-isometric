f80fc6d2e2f9e0603f59a1dc4372c0d98d167fe0
Mika Viljanen <mika.viljanen@wunderdog.fi>
2024-01-20 15:13:57 +0200

    Mimic a view-blocking obstacle by triggering a Z-index change

---
+++ TriggerZIndex.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+# This trigger changes one of the obstacles to be completely view-blocking. A trigger is needed
+# because the player object has two sprites with different Z-index values for one of them "glowing"
+# through another obstacle. In a real scenario all obstacles would use one of these approaches and
+# it could be handled with Z-indexes alone — i.e. this functionality is for demonstration only.
+
+var obstacle = null
+var original_z_index = 0
+
+func _ready():
+	obstacle = get_parent()
+	original_z_index = obstacle.z_index
+
+func _on_body_entered(body):
+	if (body == %Player):
+		obstacle.z_index = 10
+
+func _on_body_exited(body):
+	if (body == %Player):
+		obstacle.z_index = original_z_index
